fc43b5c4ba63e1815c4a05ec98dc372f32c562aa
cczerwin762 <cczerwin@udel.edu>
2024-08-11 18:10:23 -0600

    updates

--- DataBuilder.py
+++ DataBuilder.py
@@ -3,30 +3,32 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 import time
-from Helper import *
+#from Helper import *
 import re
-# # url = "https://data.alpaca.markets/v1beta1/screener/stocks/most-actives?by=volume&top=10"
+#Stage - Scrape data from Urls in urls.csv
+df = pd.DataFrame()
+result = pd.read_csv('Urls.csv')
+tickers = []
+for i in range(0,len(result)):
+    url = result['url'][i]
+    attr = result['attr'][i]
+    filt = result['filter'][i]
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    match attr:
+        case 'href':
+            for tag in soup.find_all(href=re.compile("/symbols/NASDAQ")):
+                tickers.append(tag.text)
+        case _:
+            print('invalid attribute')
 
-# # headers = {"accept": "application/json"}
-
-# # response = requests.get(url, headers=headers)
-
-# # print(response.text)
-
-#Stage - Scrape data from yfinance or above/ both
-url = "https://www.wsj.com/market-data/stocks/us/movers"
-url2 = "https://www.tradingview.com/markets/stocks-usa/market-movers-gainers/"
-response = requests.get(url)
-soup = BeautifulSoup(response.content, 'html.parser')
+print(tickers)
 
 
 #Stage - Pick out tickers
-movers = [] #top movers for the day
-for tag in soup.find_all(href=re.compile("/symbols/NASDAQ")):
-    print(tag.text)
-# for tag in soup.find_all(attrs={"data-test":"quoteLink"}):
-#     print(tag.name)
-#     print('tag')
+#movers = [] #top movers for the day
+#
+
 #Stage - Analyze each ticker
 
 #Next step set threshold and determine/ buy no buy
