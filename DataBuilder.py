64da8581fbf70148c16445c12d4d1990ae9bb882
cczerwin762 <cczerwin@udel.edu>
2024-08-15 20:47:46 -0600

    New log file functionality Think about changing to two logs for DB class

--- DataBuilder.py
+++ DataBuilder.py
@@ -4,11 +4,15 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 import numpy as np
+import datetime
 #from Helper import *
 import re
 
+#Stage - Open log file
+f = open('./logs/DataBuilderLog_' + str(datetime.datetime.now()).replace(' ', '_').replace(':', '-') + '.txt','w')
+f.write('DataBuilder.py log data for: ' + str(datetime.datetime.now()) + '\n')
 #Stage - Scrape data from Urls in urls.csv
-result = pd.read_csv('Urls.csv')
+result = pd.read_csv('./help/urls.csv')
 tickers = []
 for i in range(0,len(result)):
     url = result['url'][i]
@@ -21,14 +25,14 @@ for i in range(0,len(result)):
             for tag in soup.find_all(href=re.compile(filt)):
                 tickers.append(tag.text)
         case _:
-            print('invalid attribute')
+            f.write('invalid attribute')
 
 #print(tickers)
 
 #Stage - Remove duplicate tickers
 if len(result)>1:
     #logic to remove duplicates
-    print('Duplicates removed')
+    f.write('Duplicates removed')
 
 #Stage - Analyze each ticker
 
@@ -39,7 +43,7 @@ DivYield = []
 PE = []
 FoS = []
 for i in range(0,len(tickers)):
-    print(tickers[i] + '\n')
+    f.write(tickers[i] + '\n')
     stock = yf.Ticker(tickers[i])
     dict = stock.info
     tempDf = pd.DataFrame.from_dict(dict,orient='index')
@@ -52,25 +56,26 @@ for i in range(0,len(tickers)):
         DY = tempDf[0][tempDf['index'] == 'fiveYearAvgDividendYield'].values[0]
         P = tempDf[0][tempDf['index'] == 'forwardPE'].values[0]
     except:
-        print(tickers[i] + ' had incomplete data\n')
+        f.write(tickers[i] + ' had incomplete data\n')
         incomplete.append(i)
     EPSGrowth.append(EPSG)
     DivYield.append(DY)
     PE.append(P)
     FoS.append((EPSG+DY)/P)
-print('\n-----------------------------------------------------------\n')
-print(str(len(incomplete)) + ' out of ' + str(len(tickers)) + ' tickers being deleted.')
+f.write('\n-----------------------------------------------------------\n')
+f.write(str(len(incomplete)) + ' out of ' + str(len(tickers)) + ' tickers being deleted.')
 for i in range(0,len(incomplete)):
-    print("Removing: " + tickers[incomplete[i]-i]+ '\n')
+    f.write("Removing: " + tickers[incomplete[i]-i]+ '\n')
     del tickers[incomplete[i]-i] # -i b/c index will continuously decrease by 1 as we move through the list
     del EPSGrowth[incomplete[i]-i]
     del DivYield[incomplete[i]-i]
     del PE[incomplete[i]-i]
     del FoS[incomplete[i]-i]
-print('\n-----------------------------------------------------------\n')
+f.write('\n-----------------------------------------------------------\n')
 d = {'Ticker': tickers, 'EPSGrowth' : EPSGrowth, 'DivYield':DivYield, 'P/E':PE,'FoS':FoS}
 df = pd.DataFrame(data=d)
-print(df)
+f.write(df.to_string())
+
 # microsoft = yf.Ticker('MSFT')
 # dict =  microsoft.info
 # dfi = pd.DataFrame.from_dict(dict,orient='index')
@@ -82,3 +87,4 @@ print(df)
 #Next step set threshold and determine/ buy no buy
 
 #Look at Stocks already purchased and determine buy/ no buy/ sell
+f.close()
